1bf6f2d97d6fd8c95318147c7856c6877a4187ae
Michael wang <wangm1245@my.sduhsd.net>
2020-12-19 06:04:26 +0000

    updated dlstatus.sh functional

--- dlstatus.sh
+++ dlstatus.sh
@@ -1,1 +1,14 @@
-djsjjksjgsdg
+#!/bin/bash
+
+USERS="dl01 dl02 dl03 dl04 dl05 dl06 dl07 dl08 dl09 dl10 dl11"
+
+for user in $USERS; do
+ping -c1 $user
+	if [ $? -eq 0 ]; then
+		echo $user" : alive";
+	else
+		echo $user" : dead";
+	fi
+done
+
+exit 0
